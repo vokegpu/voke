2c74f1645093e720508ee91b503a41e045f4df84
Rina <diynggame@Gmail.com>
2025-04-23 01:39:46 -0300

    [ref] detailed help

--- src/cmd/help/help.cpp
+++ src/cmd/help/help.cpp
@@ -25,24 +25,33 @@ voke::flags_t voke::cmd::help::assert() {
   std::string help {
 R"(><
 
-If you could not understand how to use, visit:
-https://github.com/vokegpu/voke-docs
+For more development info visit:
+| - | https://github.com/vokegpu/voke
+| - | https://github.com/vokegpu/voke-docs
 
 Use:
   voke -v                 Output software version.
   voke --version
 
-  voke -sc *              Sync the system with a specific compiler: clang, mingw, gnu.
+  voke -sc *              Sync the system with a compiler: clang, mingw, clang-mingw, gnu.
   voke --sync-compiler *
+  voke ... -v *           Sync the system with a specific version of a compiler.
+  voke ... --version *
+  voke ... ... -b         Sync the system with a compiler pre-compiled binary, you can combine with version.
+  voke ... ... --binary
 
   voke -s *               Sync the system with a C/C++ library.
   voke --sync *
+  voke ... -v             Sync the system with a specific version of a C/C++ library.
+  voke ... --version *
+  voke ... ... -b         Sync the system with a C/C++ library pre-compiled binary, you can combine with version.
+  voke ... ... --binary
 
-  voke                    Upgrade all unsynced C/C++ libraries and compilers.
+  voke                    Upgrade all unsynced C/C++ libraries and compilers in the system.
   voke -su
   voke --sync-upgrade 
 
-  voke -r *               Remove a C/C++ library or a compiler.
+  voke -r *               Remove a C/C++ library or a compiler from the system.
   voke --remove *
 
 Version:)"
